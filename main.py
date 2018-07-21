c1f692b67ff9734ce1d0b03d6e830f9bc85a38ac
Scott Luse <scottwluse@gmail.com>
2018-07-21 13:19:47 -0700

    Time server files for activity05, port update

--- main.py
+++ main.py
@@ -22,6 +22,6 @@ def time_epoch():
     return (epoch_time)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
 
